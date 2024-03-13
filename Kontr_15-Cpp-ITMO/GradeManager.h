d69a4fe48e26a6504a9665fc0ce6cd3abd4d8265
Андрей <133521547+AndrejSH1981@users.noreply.github.com>
2024-03-13 23:20:42 +0300

    Update GradeManager.h

--- Kontr_15-Cpp-ITMO/GradeManager.h
+++ Kontr_15-Cpp-ITMO/GradeManager.h
@@ -2,21 +2,20 @@
 #include <map>
 #include <string>
 
-struct StudentGrade {
-    std::string student_name;
-    int grade;
+using namespace std;
 
+struct StudentGrade {
+    string student_name;
+    int grade;
 };
 
 
 class GradeManager {
 private:
-    std::map<std::string, StudentGrade> grade_by_student_name;
+    map<string, StudentGrade> grade_by_student_name;
 
 public:
-    int addStudentGrade(const std::string& student_name, int grade);
-
+    int addStudentGrade(const string& student_name, int grade);
         
-    StudentGrade* findStudentGrade(const std::string& student_name);
-
+    StudentGrade* findStudentGrade(const string& student_name);
 };
